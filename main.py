7601268d7fb110159b26659e540e8419fd44798e
Søilad <joshtgm@icloud.com>
2024-11-06 01:27:23 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 from requests import get
-from random import choice
+from random import randint
 import re
 import io
 import requests
@@ -8,32 +8,32 @@ import pytesseract
 from PIL import Image
 
 TAG_RE = re.compile(r'<[^>]+>')
-# links = ['https://www.learncbse.in/ncert-solutions-for-class-11-chemistry-chapter-1-some-basic-concepts-of-chemistry/',
-#          'https://www.learncbse.in/ncert-solutions-for-class-11-chemistry-chapter-2-structure-of-atom/',
-#          'https://www.learncbse.in/ncert-solutions-for-class-11-chemistry-chapter-3-classification-of-elements-and-periodicity-in-properties/',
-#          'https://www.learncbse.in/ncert-solutions-for-class-11-chemistry-chapter-4-chemical-bonding-and-molecular-structure/',
-#          'https://www.learncbse.in/ncert-solutions-for-class-11-chemistry-chapter-6-thermodynamics/',
-#          'https://www.learncbse.in/ncert-solutions-class-11th-physics-chapter-2-units-measurements/',
-#          'https://www.learncbse.in/ncert-solutions-class-11th-physics-chapter-3-motion-straight-line/',
-#          'https://www.learncbse.in/ncert-solutions-class-11th-physics-chapter-4-motion-plane/',
-#          'https://www.learncbse.in/ncert-class-11-solutions-physics-5-laws-motion/',
-#          'https://www.learncbse.in/ncert-solutions-class-11-physics-chapter-6-work-energy-power/',
-#          'https://www.learncbse.in/ncert-solutions-class-11-physics-chapter-7-system-particles-rotational-motion/',
-#          'https://www.learncbse.in/ncert-class-11-solutions-physics-chapter-8-gravitation/',
-#          'https://www.learncbse.in/cbse-class-11-physics-ncert-solutions-chapter-9-mechanical-properties-solids/',
-#          'https://www.learncbse.in/cbse-maths-solutions-class-11th-chapter-1-sets/',
-#          'https://www.learncbse.in/ncert-class-11-solutions-maths-chapter-2-relations-functions/',
-#          'https://www.learncbse.in/cbse-free-ncert-solutions-for-class-11th-maths-chapter-3-trigonometric-functions/',
-#          'https://www.learncbse.in/ncert-solutions-for-class-11th-maths-chapter-5-complex-numbers-and-quadratic-equations/',
-#          'https://www.learncbse.in/free-ncert-solutions-for-class-11th-maths-chapter-6-linear-inequalities/',
-#          'https://www.learncbse.in/ncert-solutions-for-class-11th-maths-chapter-7-permutation-and-combinations/',
-#          'https://www.learncbse.in/ncert-solutions-for-class-11th-maths-chapter-8-binomial-theorem/',
-#          'https://www.learncbse.in/ncert-solutions-for-class-11th-maths-chapter-9-sequences-and-series/',
-#          'https://www.learncbse.in/free-ncert-solutions-class-11th-maths-chapter-10-straight-lines/']
-#
+links = ['https://www.learncbse.in/ncert-solutions-for-class-11-chemistry-chapter-1-some-basic-concepts-of-chemistry/',
+         'https://www.learncbse.in/ncert-solutions-for-class-11-chemistry-chapter-2-structure-of-atom/',
+         'https://www.learncbse.in/ncert-solutions-for-class-11-chemistry-chapter-3-classification-of-elements-and-periodicity-in-properties/',
+         'https://www.learncbse.in/ncert-solutions-for-class-11-chemistry-chapter-4-chemical-bonding-and-molecular-structure/',
+         'https://www.learncbse.in/ncert-solutions-for-class-11-chemistry-chapter-6-thermodynamics/',
+         'https://www.learncbse.in/ncert-solutions-class-11th-physics-chapter-2-units-measurements/',
+         'https://www.learncbse.in/ncert-solutions-class-11th-physics-chapter-3-motion-straight-line/',
+         'https://www.learncbse.in/ncert-solutions-class-11th-physics-chapter-4-motion-plane/',
+         'https://www.learncbse.in/ncert-class-11-solutions-physics-5-laws-motion/',
+         'https://www.learncbse.in/ncert-solutions-class-11-physics-chapter-6-work-energy-power/',
+         'https://www.learncbse.in/ncert-solutions-class-11-physics-chapter-7-system-particles-rotational-motion/',
+         'https://www.learncbse.in/ncert-class-11-solutions-physics-chapter-8-gravitation/',
+         'https://www.learncbse.in/cbse-class-11-physics-ncert-solutions-chapter-9-mechanical-properties-solids/',
+         'https://www.learncbse.in/cbse-maths-solutions-class-11th-chapter-1-sets/',
+         'https://www.learncbse.in/ncert-class-11-solutions-maths-chapter-2-relations-functions/',
+         'https://www.learncbse.in/cbse-free-ncert-solutions-for-class-11th-maths-chapter-3-trigonometric-functions/',
+         'https://www.learncbse.in/ncert-solutions-for-class-11th-maths-chapter-5-complex-numbers-and-quadratic-equations/',
+         'https://www.learncbse.in/free-ncert-solutions-for-class-11th-maths-chapter-6-linear-inequalities/',
+         'https://www.learncbse.in/ncert-solutions-for-class-11th-maths-chapter-7-permutation-and-combinations/',
+         'https://www.learncbse.in/ncert-solutions-for-class-11th-maths-chapter-8-binomial-theorem/',
+         'https://www.learncbse.in/ncert-solutions-for-class-11th-maths-chapter-9-sequences-and-series/',
+         'https://www.learncbse.in/free-ncert-solutions-class-11th-maths-chapter-10-straight-lines/']
+
 # links = ['https://www.learncbse.in/ncert-class-11-solutions-physics-chapter-8-gravitation/']
 
-links = ['https://www.learninsta.com/class-11-physics-important-questions-chapter-2/',
+links += ['https://www.learninsta.com/class-11-physics-important-questions-chapter-2/',
          'https://www.learninsta.com/class-11-physics-important-questions-chapter-3/',
          'https://www.learninsta.com/class-11-physics-important-questions-chapter-4/',
          'https://www.learninsta.com/class-11-physics-important-questions-chapter-5/',
@@ -70,14 +70,14 @@ def main():
         # r = BeautifulSoup(get('https://www.learncbse.in/ncert-solutions-for-class-11-chemistry-chapter-4-chemical-bonding-and-molecular-structure/').content, 'html.parser').find_all('p')
         web = BeautifulSoup(get(link).content, 'html.parser')
         r = web.find_all('p')
-        pain = web.find_all('ol')
-        pain2 =web.find_all('ul')
+        pain1 = web.find_all('ol')
+        pain2 = web.find_all('ul')
         # print(r)
         for i in r:
             # print(aeg(str(i)))
             # input()
             if aeg(str(i)).strip().split('Maths ')[-1].startswith('Question'):
-                splitit = str(i).replace('Ans.','Answer:').split('Answer:')
+                splitit = str(i).replace('Ans','Answer').split('Answer')
                 q,a = aeg(splitit[0]),aeg(splitit[-1])
                 if len(splitit[-1].split('src="')) > 1:
                     a = a + '\n' + fuckimages(splitit[-1].split('src="')[1].split('"')[0])
@@ -91,19 +91,18 @@ def main():
             else:
                 idkq.append(aeg(str(i)))
     print(f'mismatched questions:{len(idkq)} mismatched answers:{len(idka)}')
-    with open(r'/home/soi/Documents/bigbrain9000/pyqdict.txt','w') as d:
+    with open(r'/home/soi/Documents/bigbrain9000/cdict.txt','w') as d:
         d.write(str(qna))
     return qna,idkq,idka
 
 if __name__ == '__main__':
     # qna,idkq,idka = main()
-    with open(r'/home/soi/Documents/bigbrain9000/dict.txt') as s:
+    with open(r'/home/soi/Documents/bigbrain9000/cdict.txt') as s:
         qna = eval(s.read())
-    with open(r'/home/soi/Documents/bigbrain9000/pyqdict.txt') as s:
-        qna = qna | eval(s.read())
     print(f'{len(qna.keys())}')
+    q = list(qna.keys())
     while 1:
-        x = choice(list(qna.keys()))
+        x = q.pop(randint(0,len(q)-1))
         print(f'\n{x}')
         input('>>>')
         print(qna[x])
